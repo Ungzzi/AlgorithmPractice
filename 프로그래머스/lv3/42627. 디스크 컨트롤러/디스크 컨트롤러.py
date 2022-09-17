7e617e2c9ff35208defb649f6fe0c46402011649
JEONG JIUNG <qhshqhsh0225@gmail.com>
2022-09-17 18:41:32 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,35 @@
+import heapq
+from collections import deque
+
+
+def solution(jobs):
+    complete = 0
+    working = False
+    h = []
+    heapq.heapify(h)
+    t = 0
+    jobs.sort()
+    jobs_q = deque(jobs)
+    while jobs_q:
+
+        for i in range(len(jobs_q)):
+            if t == jobs_q[i][0]:
+                heapq.heappush(h, (jobs_q[i][1], jobs_q[i][0]))
+
+        if h and working == False:
+            work_h = heapq.heappop(h)
+            work = work_h[0]
+            work_t = 1
+            working = True
+            complete += (t - work_h[1]) + work
+
+        if working == True:
+            if work_t == work:
+                working = False
+                jobs_q.popleft()
+
+            work_t += 1
+
+        t += 1
+
+    return complete // len(jobs)
