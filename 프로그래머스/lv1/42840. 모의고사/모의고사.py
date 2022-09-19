09fafae39f1800d2d9ae00abee10e81cdb478f81
JEONG JIUNG <qhshqhsh0225@gmail.com>
2022-09-20 00:58:36 +0900

    [level 1] Title: 모의고사, Time: 2.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -7,19 +7,13 @@ def solution(answers):
     max_win = 0
 
     for i in range(len(answers)):
-        if i < 5 and a[i] == answers[i]:
-            cnt[0] += 1
-        elif a[i % 5] == answers[i]:
+        if a[i % 5] == answers[i]:
             cnt[0] += 1
 
-        if i < 8 and b[i] == answers[i]:
-            cnt[1] += 1
-        elif b[i % 8] == answers[i]:
+        if b[i % 8] == answers[i]:
             cnt[1] += 1
 
-        if i < 10 and c[i] == answers[i]:
-            cnt[2] += 1
-        elif c[i % 10] == answers[i]:
+        if c[i % 10] == answers[i]:
             cnt[2] += 1
 
     for i in range(len(cnt)):
