7459ecc3b41d76a89684e642c20dee322cb234cf
JEONG JIUNG <qhshqhsh0225@gmail.com>
2022-09-29 00:48:06 +0900

    [Silver II] Title: 최소 힙, Time: 160 ms, Memory: 36764 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+
+h = []
+heapq.heapify(h)
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    op = int(sys.stdin.readline())
+    if op == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(h))
+    else:
+        heapq.heappush(h, op)
