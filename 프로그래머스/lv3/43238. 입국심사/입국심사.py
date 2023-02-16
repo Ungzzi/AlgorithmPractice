125f67db4c7feb4644f15d6fd0b1e889b2047570
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-02-16 20:57:41 +0900

    [level 3] Title: 입국심사, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,12 +1,11 @@
 def solution(n, times):
-    answer = 0
-
     start = min(times)
     end = n * max(times)  # 가장 비효율적일 때
 
     # 총 심사 시간 기준 이분탐색
     while start <= end:
         mid = (start + end) // 2
+        print(mid)
         people = 0  # 심사 받은 사람 수
 
         for time in times:
@@ -16,10 +15,9 @@ def solution(n, times):
 
         # 심사한 사람 수가 n보다 많거나 같은 경우
         if people >= n:
-            answer = mid
             end = mid - 1
         # 심사한 사람 수가 n보다 적은 경우
-        elif people <= n:
+        elif people < n:
             start = mid + 1
 
     return start
