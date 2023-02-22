d16a56fdb8075aaddd19c5b1a9ad2f5a148fbcce
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-02-22 23:49:58 +0900

    [level 3] Title: 순위, Time: 150.01 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.py
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -1,33 +1,12 @@
 def solution(n, results):
     answer = 0
-    win = [[0] * n for _ in range(n)]
-    lose = [[0] * n for _ in range(n)]
-    
+    graph = [[0] * n for _ in range(n)]
     for x, y in results:
-        win[x-1][y-1] = 1
-        lose[y-1][x-1] = 1
-        
+        graph[x-1][y-1] = 1
     for i in range(n):
         for j in range(n):
             for k in range(n):
-                if (win[j][i] == 1 and win[i][k]):
-                    win[j][k] = 1
-
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                if (lose[j][i] == 1 and lose[i][k]):
-                    lose[j][k] = 1
-
-
-    for i in range(n):
-        temp = 0
-        for j in range(n):
-            if (win[i][j] == 0 and lose[i][j] == 0 and i != j):
-                break
-            else:
-                temp += 1
-        if temp == n:
-            answer += 1
-
+                if (graph[j][i] == 1 and graph[i][k]):
+                    graph[j][k] = 1
+    print(graph)
     return answer
