a1d7e34354ba358f3f6d070e014b47996cad7b46
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-09-13 23:37:37 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+public class Main {
+    public void solution(){
+        String str = "         ,r'\"7\n" +
+                     "r`-_   ,'  ,/\n" +
+                     " \\. \". L_r'\n" +
+                     "   `~\\/\n" +
+                    "      |\n" +
+                    "      |";
+        System.out.println(str);
+    }
+    public static void main(String[] args){
+        new Main().solution();
+    }
+}
