76ecb010e22ec88b7bfc8a5f9f3def051942fe82
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-09-14 15:00:51 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum = 1;
+        int ans = 1;
+
+        while (sum < N) {sum += 6 * ans++;}
+        System.out.println(ans);
+    }
+}
