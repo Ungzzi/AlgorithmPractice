3229934d1e5fc752f12e24c430321e5a1c75ed02
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-09-14 15:36:24 +0900

    [Silver V] Title: 영화감독 숌, Time: 304 ms, Memory: 85820 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int num = 665;
+        int cnt = 0;
+
+        while (N != cnt) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                cnt++;
+            }
+        }
+        System.out.println(num);
+    }
+}
