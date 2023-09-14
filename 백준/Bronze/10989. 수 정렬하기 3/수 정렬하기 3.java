7836f47be65df9e2b8b460108707d2ef906a7aa6
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-09-14 15:05:03 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2724 ms, Memory: 417880 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
