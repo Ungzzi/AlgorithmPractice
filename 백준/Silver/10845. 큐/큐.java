1087691edd5e1cff7d255808470ab151db7d7574
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-09-14 09:40:16 +0900

    [Silver IV] Title: 큐, Time: 380 ms, Memory: 19808 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Queue<Integer> q = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int rear = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String ord = st.nextToken();
+
+            switch (ord) {
+                case "push":
+                    int num = Integer.parseInt(st.nextToken());
+                    q.add(num);
+                    rear = num;
+                    break;
+                case "pop":
+                    if (q.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(q.poll());
+                    }
+                    break;
+                case "size":
+                    System.out.println(q.size());
+                    break;
+                case "empty":
+                    if (q.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "front":
+                    if (q.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(q.peek());
+                    }
+                    break;
+                case "back":
+                    if (q.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(rear);
+                    }
+                    break;
+            }
+        }
+    }
+}
