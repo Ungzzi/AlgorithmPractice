0dbb3e8d938978f052b1932efd362acca7ce3ab6
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-09-15 10:25:13 +0900

    [Bronze II] Title: Hashing, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        int sum = 0;
+        
+        for (int i = 0; i < N; i++) {
+            char c = str.charAt(i);
+            int num = c - 96;
+            sum += num * Math.pow(31, i);
+        }
+
+        System.out.println(sum);
+
+    }
+}
