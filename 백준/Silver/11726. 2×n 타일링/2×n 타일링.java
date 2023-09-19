1d68947fd3972d6d22c30915ce9f7a80a6fe04a8
JEONG JIUNG <qhshqhsh0225@gmail.com>
2023-09-19 11:13:53 +0900

    [Silver III] Title: 2×n 타일링, Time: 216 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -2,27 +2,18 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    static long[] arr;
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        arr = new long[N+1];
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
 
-        for (int i = 0; i < N + 1; i++) {
-            arr[i] = -1;
+        for (int i = 3; i < N+1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
 
-        arr[0] = 1;
-        arr[1] = 1;
-
-        System.out.println(fib(N));
-    }
-
-    public static long fib(int N) {
-        if (arr[N] == -1) {
-            arr[N] = fib(N - 1) + fib(N - 2);
-        }
-        return arr[N] % 10007;
+        System.out.println(dp[N]);
     }
 }
