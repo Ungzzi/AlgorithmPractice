bc805fe769f3505df9f0eec7969b6ac1a4edc068
JEONG JIUNG <qhshqhsh0225@gmail.com>
2024-12-18 16:48:35 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 22424 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,36 +1,45 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static boolean[] visit; static int[] num;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+    static StringBuilder sb;
+    static boolean[] isUsed;
+    static int[] arr;
+    static int M, N;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        visit = new boolean[N + 1];
-        num = new int[N + 1];
 
-        backTracking(0);
+        isUsed = new boolean[N + 1];
+        arr = new int[M];
+
+        go(0);
 
         System.out.println(sb);
     }
-    public static void backTracking(int cnt) {
-        if (cnt == M){
-            for(int i = 0; i<M; i++){
-                sb.append(num[i]).append(" ");
+
+    static void go(int dep) {
+        if (dep == M) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for(int i = 1; i <= N; i++){
-            if(visit[i]) continue;
-            visit[i] = true;
-            num[cnt] = i;
-            backTracking(cnt + 1);
-            visit[i] = false;
+
+        for (int i = 1; i <= N; i++) {
+            if (!isUsed[i]) {
+                isUsed[i] = true;
+                arr[dep] = i;
+                go(dep + 1);
+                isUsed[i] = false;
+            }
         }
     }
 }
