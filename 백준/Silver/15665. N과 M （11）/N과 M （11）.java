0b6d291ad95f7f3fc85a8e1e74a4acf2af93be1a
JEONG JIUNG <qhshqhsh0225@gmail.com>
2024-12-19 10:29:02 +0900

    [Silver II] Title: N과 M (11), Time: 328 ms, Memory: 33676 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int arr[];
+    static int store[];
+    static boolean isUsed[];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        store = new int[M];
+        isUsed = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        go(0);
+
+        System.out.println(sb);
+    }
+
+    static void go(int dep) {
+        if (dep == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(store[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = -1;
+
+        for (int i = 0; i < N; i++) {
+            if (before != arr[i]) {
+                store[dep] = arr[i];
+                before = arr[i];
+                go(dep + 1);
+            }
+        }
+    }
+
+}
